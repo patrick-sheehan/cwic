5e2b5e1916530df9703114b63c230a30ed8b61e5
Patrick Sheehan <psheehan0301@gmail.com>
2017-07-01 18:58:23 -1000

    Update AppDelegate with appwide color and font appearance

--- ios/Source/AppDelegate.swift
+++ ios/Source/AppDelegate.swift
@@ -8,44 +8,48 @@
 
 import UIKit
 
+
+enum Colors {
+  static let Red = UIColor(netHex: 0xCE171F)
+  static let Black = UIColor(netHex: 0x040707)
+  static let RoyalBlue = UIColor(netHex: 0x1F11A5)
+}
+
+enum Fonts {
+  static let Lucida = UIFont(name: "LucidaGrande", size: 16.0)!
+}
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
-
-
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
     
+    /// Configure navigation bar
+    let navigationBarAppearance = UINavigationBar.appearance()
+    navigationBarAppearance.tintColor = .white
+    navigationBarAppearance.barTintColor = Colors.Red
+    navigationBarAppearance.isTranslucent = false
+    navigationBarAppearance.titleTextAttributes = [
+      NSForegroundColorAttributeName: UIColor.white,
+      NSFontAttributeName: Fonts.Lucida
+    ]
+    
+    /// Configure tab bar
+    let tabBarAppearance = UITabBar.appearance()
+    tabBarAppearance.tintColor = .white
+    tabBarAppearance.barTintColor = Colors.RoyalBlue
+    
+    
+    // Configure main window
     window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = RootViewController()
+    window?.rootViewController = UINavigationController.getRootView()
     window?.makeKeyAndVisible()
     
     return true
   }
-
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-  }
-
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-
-
+  
 }
 
