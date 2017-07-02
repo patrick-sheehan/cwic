aebce186e747c1d266794bea21a4d92ca0e193f1
Patrick Sheehan <psheehan0301@gmail.com>
2017-07-01 18:58:37 -1000

    Add stubbed functionality for Profile view

--- ios/Source/ProfileViewController.swift
+++ ios/Source/ProfileViewController.swift
@@ -9,27 +9,37 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var usernameLabel: UILabel!
+  @IBOutlet weak var viewTrophiesButton: UIButton!
+  @IBOutlet weak var tableView: UITableView!
+  
+  var actions: [String] = [
+    "Edit Profile",
+    "My Tickets",
+    "My Pages",
+    "Send Push Notification",
+    "Become a Judge"
+  ]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
+    tableView.dataSource = self
+    
+    navigationItem.title = "My Profile"
+  }
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension ProfileViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return actions.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    return tableView.defaultCell(actions[indexPath.row])
+  }
 }
