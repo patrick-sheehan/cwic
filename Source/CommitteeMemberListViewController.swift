42846b56a86a2d2336805dd3c8b07e1dfe47133e
Patrick Sheehan <psheehan0301@gmail.com>
2018-01-18 15:08:05 -0600

    Add image assets

---
+++ Source/CommitteeMemberListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CommitteeMemberListViewController.swift
+//  CWIC
+//
+//  Created by Patrick Sheehan on 1/18/18.
+//  Copyright © 2018 Síocháin Solutions. All rights reserved.
+//
+
+import Foundation
