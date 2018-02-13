84f46da56aad9b3610739edb1adfeeee75e641b3
Patrick Sheehan <psheehan0301@gmail.com>
2018-02-12 21:57:30 -0600

    Position deserialization

--- Source/Position.swift
+++ Source/Position.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
-struct Position: Codable {
+struct Position {
   let lat: Double
   let lon: Double
+  
+  init?(json: [String: Any]) {
+    
+    PrinterService.log(json.debugDescription)
+    guard let lat = json["lat"] as? Double,
+      let lon = json["lon"] as? Double else {
+        return nil
+    }
+    
+    self.lat = lat
+    self.lon = lon
+  }
 }
+
