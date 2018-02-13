55e36972763cf0c78675d192c5ab32f6db129d21
Patrick Sheehan <psheehan0301@gmail.com>
2018-02-12 21:55:58 -0600

    TODO notes in About VC

--- Source/AboutCwicViewController.swift
+++ Source/AboutCwicViewController.swift
@@ -52,11 +52,11 @@ class AboutCwicViewController: UIViewController {
   }
   
   @objc func aboutCharityAction() {
-    
+    self.showPopupError("Whoops! Charity info coming soon!")
   }
   
   @objc func aboutCookoffAction() {
-    
+    self.showPopupError("Whoops! Cook-off info coming soon!")
   }
   
   @objc func facebookAction() {
@@ -88,6 +88,13 @@ class AboutCwicViewController: UIViewController {
   }
   
   @objc func mailingListAction() {
-    
+    self.showPopupError("Whoops! Mailing list subscription coming soon!")
   }
 }
+
+
+
+
+
+//Did you know..... 22 soldiers KBS per day. Whats KBS? Killed by suicide. 22 per DAY. The root cause is that a vet returning from a war zone simply has no tools to cope with the fact that they aren't in a war zone any more. They are unable to transition. So this year we,The Chili When Its Chilly Cook-Off Committee, have decided to raise awareness and raise money for The ReBoot Program with the Boot Campaign, which tackles the root cause of the struggles affecting so many of our brave servicemen and women, and brings together world class mental and physical treatment for them. Making them whole again. Thats 22 Moms and Dads who WON'T suffer the loss of their soldier by suicide. I want that number to be ZERO and I hope you do too. CWIC will always work to fulfill a solitary goal of helping folks who need it. Being blessed to be a part of doing good for others is a calling we accept, so the Board of Directors of A MAJOR FUNDING with the Executive Committee for CWIC will continue to seek out and work with charitable organizations who share our vision of grass-roots efforts to directly help those who need it, with as little overhead as possible to ensure the donation is put to good use. We remain committed to our cause, and are appreciative of the sponsors, supporters, and participants who share in our mission to help those in need. ALL PROFITS GO TO THE CHARITY, there are no paid staff associated with any part of this event.
+
